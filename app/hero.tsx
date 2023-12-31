7423e4452e7bf1c5bae5f695fb80631b744d9861
sagarmusabbir <sagarmusabbir@gmail.com>
2023-12-31 23:13:03 +0600

    fixed

--- app/hero.tsx
+++ app/hero.tsx
@@ -14,7 +14,7 @@ const DefaultHero: FC = function () {
           </span>
           &nbsp;
           <span className="text-sm font-medium">
-            Flowbite is out&apos; See what's new
+            Flowbite is out See what is new
           </span>
           <svg
             className="ml-2 h-5 w-5"
